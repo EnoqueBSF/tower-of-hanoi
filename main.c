2294824db61f8217d0a72eab5b5a93224da257e6
Emerson Bezerra <emersonbioinfo@gmail.com>
2019-11-06 19:01:01 -0300

    arquivo main.c criado com a descrição sobre objetivos e regras da torre de hanói

---
+++ main.c
@@ -0,0 +1,21 @@
+/* Torre de Hanói é um "quebra-cabeça" que consiste em uma base contendo três pinos, \
+em um dos quais são dispostos alguns discos uns sobre os outros, em ordem crescente de diâmetro, \
+de cima para baixo. O problema consiste em passar todos os discos de um pino para outro qualquer, \
+usando um dos pinos como auxiliar, de maneira que um disco maior nunca fique em cima de outro \
+menor em nenhuma situação. O número de discos pode variar sendo que o mais simples contém apenas três.
+
+De forma explícita, portanto, as regras a serem implementadas são:
+
+> Movimentar uma só peça (disco) de cada vez.
+> Uma peça maior não pode ficar acima de uma menor.
+> Não é permitido movimentar uma peça que esteja abaixo de outra.
+
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+
+    return 0;
+}
